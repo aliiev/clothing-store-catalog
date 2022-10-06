40afeca63d82a231d76cc69e8ee893ec523b43d1
aliiev <aliiev.akhtem@gmail.com>
2022-10-06 17:38:49 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,11 @@
+import './styles/App.scss'
+
+export default function App() {
+  return (
+    <div className="container mt-3">
+      <h1>Hello world</h1>
+      <input type="text" className="form-control mb-2" placeholder="Enter text" />
+      <button className="btn btn-darkgray">Button</button>
+    </div>
+  )
+}
